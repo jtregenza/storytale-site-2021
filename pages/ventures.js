0a5a2a717bafb603e8a6a46ff8303bbc745e4f2a
Josh Tregenza <josh@joshtregenza.com>
2021-08-25 17:25:56 +1000

    Update ventures.js

--- pages/ventures.js
+++ pages/ventures.js
@@ -1,14 +1,14 @@
 import Layout from "../components/layout";
-import link from "next/link";
+import Link from "next/link";
 
 export default function Ventures() {
 
 return(
 	<Layout>
 		<ul>
-			<li><Link href="/seasonal">Seasonal</Link></li>
-			<li><Link href="/fables">Fables</Link></li>
-			<li><Link href="/crumple">Crumple</Link></li>
+			<li><Link href="./seasonal">Seasonal</Link></li>
+			<li><Link href="./fables">Fables</Link></li>
+			<li><Link href="./crumple">Crumple</Link></li>
 		</ul>
 	</Layout>
 )
