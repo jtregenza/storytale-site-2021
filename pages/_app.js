d1cc3149c624d1e014b251196ef0900db7a9e54c
Josh Tregenza <josh@joshtregenza.com>
2021-08-25 18:55:17 +1000

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -7,20 +7,7 @@ export default function App({ Component, pageProps, router }) {
 
   return (
     <AnimatePresence exitBeforeEnter>
-       <motion.div key={router.route} initial="pageInitial" animate="pageAnimate" exit="pageExit" variants={{
-        pageInitial: {
-          opacity: 0,
-          scale: 0.8
-        },
-        pageAnimate: {
-          opacity: 1,
-          scale: 1
-        },
-        pageExit: {
-          opacity: 0,
-          scale: 0.8
-        }
-      }}>
+       <motion.div key={router.route} >
         <Component {...pageProps} />
       </motion.div>
     </AnimatePresence>
