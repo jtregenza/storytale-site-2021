9b7028835bcb0658f4d17e91a078655ccc37fbf2
Josh Tregenza <josh@joshtregenza.com>
2021-09-27 09:54:44 +1000

    google analytics set up

--- components/layout.js
+++ components/layout.js
@@ -85,6 +85,24 @@ export default function Layout({ children, home, type }) {
         <meta name="twitter:site" content="@StudioStorytale"/>
         <meta name="twitter:image:alt" content="Stortale Studio Twitter Card Image" />
         <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+        <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
+
+
       </Head>
 
       
