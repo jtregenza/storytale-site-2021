8bfc76e1b06e172305150875a8c5e6835e884834
Josh Tregenza <josh@joshtregenza.com>
2021-10-27 13:16:10 +1000

    Update sitemap.xml.js

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -12,7 +12,7 @@ export const getServerSideProps = ({ res }) => {
   const staticPages = fs
   .readdirSync({
     development: 'pages',
-    production: 'var/runtime/',
+    production: './var/runtime/',
   }[process.env.NODE_ENV])
   .filter((staticPage) => {
     return ![
