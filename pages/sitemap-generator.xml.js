d19107fa0fbd1ff1dfdcec511d956a11c2291426
Josh Tregenza <josh@joshtregenza.com>
2021-10-28 13:22:48 +1000

    update

---
+++ pages/sitemap-generator.xml.js
@@ -0,0 +1,57 @@
+
+import fs from "fs";
+
+const Sitemap = () => {};
+
+export const getServerSideProps = ({ res }) => {
+  const baseUrl = {
+    development: "http://localhost:3000",
+    production: "https://storytale.design",
+  }[process.env.NODE_ENV];
+
+  const staticPages = fs
+  .readdirSync({
+    development: 'pages',
+    production: './',
+  }[process.env.NODE_ENV])
+  .filter((staticPage) => {
+    return ![
+      "_app.js",
+      "_document.js",
+      "_error.js",
+      "sitemap.xml.js",
+    ].includes(staticPage);
+  })
+  .map((staticPagePath) => {
+    return `${baseUrl}/${staticPagePath}`;
+  });
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${staticPages
+        .map((url) => {
+          return `
+            <url>
+              <loc>${url}</loc>
+              <lastmod>${new Date().toISOString()}</lastmod>
+              <changefreq>monthly</changefreq>
+              <priority>1.0</priority>
+            </url>
+          `;
+        })
+        .join("")}
+    </urlset>
+  `;
+
+
+  res.setHeader("Content-Type", "text/xml");
+  res.write(sitemap);
+  res.end();
+  fs.writeFile('public/sitemap.xml', sitemap, err => { if (err) { console.error(err); return } })
+  return {
+    props: {},
+  };
+
+};
+
+export default Sitemap;
