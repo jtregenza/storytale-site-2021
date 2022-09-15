5387e7a045db68d292c8b2b612364d387f34453d
Josh Tregenza <josh@joshtregenza.com>
2022-09-16 09:42:03 +1000

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -8,20 +8,20 @@ import * as ga from '../lib/ga'
 
 export default function App({ Component, pageProps, router }) {
 
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      ga.pageview(url)
-    }
-    //When the component is mounted, subscribe to router changes
-    //and log those page views
-    router.events.on('routeChangeComplete', handleRouteChange)
+  // useEffect(() => {
+  //   const handleRouteChange = (url) => {
+  //     ga.pageview(url)
+  //   }
+  //   //When the component is mounted, subscribe to router changes
+  //   //and log those page views
+  //   router.events.on('routeChangeComplete', handleRouteChange)
 
-    // If the component is unmounted, unsubscribe
-    // from the event with the `off` method
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
+  //   // If the component is unmounted, unsubscribe
+  //   // from the event with the `off` method
+  //   return () => {
+  //     router.events.off('routeChangeComplete', handleRouteChange)
+  //   }
+  // }, [router.events])
 
 
   return (
